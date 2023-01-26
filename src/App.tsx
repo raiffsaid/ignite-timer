677f4824f5bc4853bb56b8fd7c5a49dc37e112d6
Raiff Said <raiff.said@gmail.com>
2023-01-26 16:10:57 -0300

    feat: Project styles and ESLint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="danger" />
+        <Button variant="success" />
+        <Button />
+
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   );
 }
