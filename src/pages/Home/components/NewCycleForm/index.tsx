3c710401d3143ff49fffdc8c34e37044549eef73
Raiff Said <raiff.said@gmail.com>
2023-01-30 18:00:18 -0300

    feat: Context provider between Routes

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,9 +1,7 @@
 import { FormContainer, TaskInput, MinutesAmountInput } from './styles'
-import * as zod from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm, useFormContext } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 import { useContext } from 'react';
-import { CyclesContext } from '../..';
+import { CyclesContext } from '../../../../contexts/CyclesContext';
 
 export function NewCycleForm() {
   const { activeCycle } = useContext(CyclesContext);
